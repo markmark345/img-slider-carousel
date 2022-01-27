375e4fe0627281ca199342d6da01f2f59ad2f6af
markmark345 <natapatchara.anuroje@gmail.com>
2022-01-28 03:06:22 +0700

    set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="relative z-10 w-full mx-auto sm:max-w-screen-md mt-6">
+        <Slider/>
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav class="flex items-center justify-between flex-wrap bg-gray-500 p-4">
+      <div class="flex items-center flex-shrink-0 text-white mr-6 ml-16">
+        <text class="font-blod text-lg">Image Slider</text>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
