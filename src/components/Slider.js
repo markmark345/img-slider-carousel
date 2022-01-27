abb797dcbcce5eaf9fbb448fe93c12fe303f8af0
markmark345 <natapatchara.anuroje@gmail.com>
2022-01-28 03:06:11 +0700

    set up

---
+++ src/components/Slider.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from "react";
+import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from "react-icons/ai";
+
+let count = 0;
+let slider;
+
+const imgCat = [
+  "/cat/cat-1.jpg",
+  "/cat/cat-2.jpg",
+  "/cat/cat-3.jpg",
+];
+
+const Slider = () => {
+    return (
+        <div className="w-full select-none relative">
+            <div className="">
+
+            </div>
+            <h1>Slider</h1>
+        </div>
+    )
+}
+
+export default Slider
